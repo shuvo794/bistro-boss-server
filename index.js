c7f8899c819c672f1604c11fd112d20b4b3731e3
shuvo794 <shuvohossain794@gmail.com>
2023-12-21 08:26:08 +0600

    added server code

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express=require('express')
 const app =express(express);
 const cors = require('cors');
+const port = process.env.PORT || 5000;
 
 // middile ware
 
 app.use(cors())
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('boss is setting on chair')
+})
+
+app.listen(port,()=>{
+    console.log(`bistro boos is setting ${port}`);
+})
