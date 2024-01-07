d1c6126adb133376451eb470181dcbb21b22d9d1
shuvo794 <shuvohossain794@gmail.com>
2024-01-07 21:25:42 +0600

    send code

--- index.js
+++ index.js
@@ -143,6 +143,7 @@ async function run() {
     // cart collection api
 
     // Todo Error Get this line
+    
     app.get("/carts", verifyToken, async (req, res) => {
       const email = req.query.email;
       if (!email) {
