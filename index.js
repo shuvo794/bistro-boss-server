a2100b292c812bb83ebf379d4a4241222e6643af
shuvo794 <shuvohossain794@gmail.com>
2024-01-16 07:51:02 +0600

    added admin route

--- index.js
+++ index.js
@@ -139,6 +139,13 @@ async function run() {
       res.send(result);
     });
 
+app.delete("/menu/:id",verifyToken,verifyAdmin, async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await menuCollection.deleteOne(query);
+  res.send(result);
+});
+
     // review related apis
     app.get("/review", async (req, res) => {
       const result = await reviweCollection.find().toArray();
