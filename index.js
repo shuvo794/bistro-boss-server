d803c2e8ff19be547b965dfc3f59d9f972783bc9
shuvo794 <shuvohossain794@gmail.com>
2024-01-18 09:28:45 +0600

    send

--- index.js
+++ index.js
@@ -192,13 +192,12 @@ async function run() {
 
     // payments api create
 
-    app.post("/payments", verifyToken, async (req, res) => {
+    app.post("/payments",verifyToken, async (req, res) => {
       const payment = req.body;
       const result = await paymentCollection.insertOne(payment);
 
-      const query = {
-        _id: { $in: payment.cartItems.map((id) => new ObjectId(id)) },
-      };
+      const query = { _id: { $in: payment.cartItems.map(id => new ObjectId(id)) } };
+      const resultDelete = await cartCollection.deleteMany(query);
       res.send(result);
     });
 
