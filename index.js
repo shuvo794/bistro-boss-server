1996d994b0d4246857a7315b4d6111b5635a1ef3
shuvo794 <shuvohossain794@gmail.com>
2024-01-22 12:24:53 +0600

    send solve code

--- index.js
+++ index.js
@@ -191,7 +191,7 @@ async function run() {
     // Create payment intent
     app.post("/create-payment-intent", verifyToken, async (req, res) => {
       const { price } = req.body;
-      const amount = price * 100;
+      const amount = parseInt(price * 100);
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
         currency: "usd",
