54bfec42af10abd0a8479c078a54c1a7c493570e
shuvo794 <shuvohossain794@gmail.com>
2024-01-30 10:11:15 +0600

    install a nodemailer

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 require("dotenv").config();
 const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
