138d51cb33405cec3bb64452c24ed4611240fda4
shuvo794 <shuvohossain794@gmail.com>
2024-01-30 10:21:18 +0600

    install a nodemailer

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// send email confermation email
+
+const sendEmailConfermationEmail = payment => {
+  
+}
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8g1rh.mongodb.net/?retryWrites=true&w=majority`;
 
